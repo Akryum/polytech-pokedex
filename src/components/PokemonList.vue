3d2f553d0c049f7bf75e6ccf8ce721cbd2b3b018
Guillaume Chau <guillaume.b.chau@gmail.com>
2018-06-10 20:56:32 +0200

    feat: basic pokedex

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="pokemon-list">
+    <h1>Pokedex</h1>
+    <div class="toolbar">
+      <input v-model="search" placeholder="Filter...">
+    </div>
+    <div class="pokemons">
+      <PokemonListItem
+        v-for="pokemon of displayedPokemons"
+        :key="pokemon.id"
+        :pokemon="pokemon"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import PokemonListItem from './PokemonListItem'
+import pokemons from '../assets/pokemons.json'
+
+export default {
+  components: {
+    PokemonListItem
+  },
+
+  data () {
+    return {
+      pokemons,
+      search: ''
+    }
+  },
+
+  computed: {
+    displayedPokemons () {
+      if (this.search) {
+        const regex = new RegExp(this.search, 'i')
+        return this.pokemons.filter(
+          pokemon => pokemon.name.match(regex)
+        )
+      }
+      return this.pokemons
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/style/imports'
+
+.pokemon-list
+  margin $padding
+
+.pokemons
+  display grid
+  grid-template-columns repeat(auto-fill, 200px)
+  grid-auto-rows 200px
+  grid-gap 12px
+
+.toolbar
+  margin-bottom $padding
+</style>
