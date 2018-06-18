863ab7e26967a70da2a86e357507910efc7bb51f
Guillaume Chau <alphadelta.fg@gmail.com>
2018-06-18 19:09:03 +0200

    add vue-cli-plugin-apollo

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import './components'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  provide: createProvider().provide(),
   render: h => h(App)
 }).$mount('#app')
